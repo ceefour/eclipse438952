0ab8baca42131129b04e1f1ba792a95cb94b7992
Hendy Irawan <ceefour666@gmail.com>
2014-08-18 06:01:50 -0430

    simplify

--- src/main/java/com/hendyirawan/bug/eclipse438952/App.java
+++ src/main/java/com/hendyirawan/bug/eclipse438952/App.java
@@ -3,10 +3,6 @@ package com.hendyirawan.bug.eclipse438952;
 import java.util.concurrent.Executors;
 import java.util.function.Supplier;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
